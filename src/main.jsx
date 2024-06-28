0548961eb94276ce79af8148653d629f90e44b0c
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-06-27 23:09:40 -0500

    register users, display form validation errors

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,17 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store/index.js";
 import { PersistGate } from "redux-persist/integration/react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "react-toastify/dist/ReactToastify.css";
+import "react-quill/dist/quill.snow.css";
+import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<Provider store={store}>
 		<PersistGate persistor={persistor}>
+			<ToastContainer position="top-right" />
 			<App />
 		</PersistGate>
 	</Provider>
