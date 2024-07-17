756b0ca2dfb04133317482de05f6cfb1e0a4c76f
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-07-16 22:21:46 -0500

    SearchBox component

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -9,6 +9,7 @@ import {
 	setCurrentUser,
 } from "../../redux/slices/userSlice.js";
 import { toast } from "react-toastify";
+import SearchBox from "../search/SearchBox.jsx";
 
 export default function Header() {
 	const { isLoggedIn, token, user } = useSelector((state) => state.user);
@@ -55,110 +56,126 @@ export default function Header() {
 	};
 
 	return (
-		<nav className="navbar navbar-expand-lg bg-body-tertiary">
-			<div className="container-fluid">
-				<Link className="navbar-brand" to="/">
-					React Medium Clone
-				</Link>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-						<li className="nav-item">
-							<Link
-								className={`nav-link ${
-									location.pathname === "/" ? "active" : ""
-								}`}
-								aria-current="page"
-								to="/"
-							>
-								<i className="bi bi-house"></i> Home
-							</Link>
-						</li>
-						{isLoggedIn ? (
-							<>
-								<li className="nav-item">
-									<Link
-										className={`nav-link ${
-											location.pathname === "/write" ? "active" : ""
-										}`}
-										aria-current="page"
-										to="/write"
-									>
-										<i className="bi bi-pencil"></i> Write
-									</Link>
-								</li>
-								<li className="nav-item">
-									<Link
-										className={`nav-link ${
-											location.pathname === "/profile" ? "active" : ""
-										}`}
-										aria-current="page"
-										to="/profile"
-									>
-										<i className="bi bi-person"></i> {user?.name}
-									</Link>
-								</li>
-								<li className="nav-item">
-									<button
-										onClick={() => logoutUser()}
-										className="nav-link border-0 bg-light"
-										aria-current="page"
-									>
-										<i className="bi bi-person-fill-down"></i> Logout
-									</button>
-								</li>
-								<li className="nav-item">
-									<Link
-										className={`nav-link ${
-											location.pathname === "/bookmarked" ? "active" : ""
-										}`}
-										aria-current="page"
-										to="/bookmarked"
-									>
-										<i className="bi bi-bookmark-plus"></i> (
-										{bookmarked?.length})
-									</Link>
-								</li>
-							</>
-						) : (
-							<>
-								<li className="nav-item">
-									<Link
-										className={`nav-link ${
-											location.pathname === "/login" ? "active" : ""
-										}`}
-										aria-current="page"
-										to="/login"
-									>
-										<i className="bi bi-person-fill-up"></i> Login
-									</Link>
-								</li>
-								<li className="nav-item">
-									<Link
-										className={`nav-link ${
-											location.pathname === "/register" ? "active" : ""
-										}`}
-										aria-current="page"
-										to="/register"
-									>
-										<i className="bi bi-person-add"></i> Register
-									</Link>
-								</li>
-							</>
-						)}
-					</ul>
+		<>
+			<nav className="navbar navbar-expand-lg bg-body-tertiary">
+				<div className="container-fluid">
+					<Link className="navbar-brand" to="/">
+						React Medium Clone
+					</Link>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+							<li className="nav-item">
+								<Link
+									className={`nav-link ${
+										location.pathname === "/" ? "active" : ""
+									}`}
+									aria-current="page"
+									to="/"
+								>
+									<i className="bi bi-house"></i> Home
+								</Link>
+							</li>
+							{isLoggedIn ? (
+								<>
+									<li className="nav-item">
+										<Link
+											className={`nav-link ${
+												location.pathname === "/write" ? "active" : ""
+											}`}
+											aria-current="page"
+											to="/write"
+										>
+											<i className="bi bi-pencil"></i> Write
+										</Link>
+									</li>
+									<li className="nav-item">
+										<Link
+											className={`nav-link ${
+												location.pathname === "/profile" ? "active" : ""
+											}`}
+											aria-current="page"
+											to="/profile"
+										>
+											<i className="bi bi-person"></i> {user?.name}
+										</Link>
+									</li>
+									<li className="nav-item">
+										<button
+											onClick={() => logoutUser()}
+											className="nav-link border-0 bg-light"
+											aria-current="page"
+										>
+											<i className="bi bi-person-fill-down"></i> Logout
+										</button>
+									</li>
+									<li className="nav-item">
+										<Link
+											className={`nav-link ${
+												location.pathname === "/bookmarked" ? "active" : ""
+											}`}
+											aria-current="page"
+											to="/bookmarked"
+										>
+											<i className="bi bi-bookmark-plus"></i> (
+											{bookmarked?.length})
+										</Link>
+									</li>
+								</>
+							) : (
+								<>
+									<li className="nav-item">
+										<Link
+											className={`nav-link ${
+												location.pathname === "/login" ? "active" : ""
+											}`}
+											aria-current="page"
+											to="/login"
+										>
+											<i className="bi bi-person-fill-up"></i> Login
+										</Link>
+									</li>
+									<li className="nav-item">
+										<Link
+											className={`nav-link ${
+												location.pathname === "/register" ? "active" : ""
+											}`}
+											aria-current="page"
+											to="/register"
+										>
+											<i className="bi bi-person-add"></i> Register
+										</Link>
+									</li>
+								</>
+							)}
+						</ul>
+						<ul className="navbar nav ml-auto mb-2 ml-lg-0">
+							<li className="nav-item">
+								<a
+									className="nav-link"
+									data-bs-toggle="offcanvas"
+									href="#offcanvasExample"
+									role="button"
+									aria-controls="offcanvasExample"
+								>
+									<i className="bi bi-search"></i>
+								</a>
+							</li>
+						</ul>
+					</div>
 				</div>
-			</div>
-		</nav>
+			</nav>
+			<SearchBox />
+		</>
 	);
 }
---
+++ src/components/search/SearchBox.jsx
@@ -0,0 +1,108 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { BASE_URL } from "../../helpers/config";
+
+export default function SearchBox() {
+	const [searchTerm, setSearchTerm] = useState("");
+	const [message, setMessage] = useState("");
+	const [articles, setArticles] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	const searchArticles = async (e) => {
+		e.preventDefault();
+		setArticles([]);
+		setMessage("");
+		setLoading(true);
+
+		const data = { searchTerm };
+
+		try {
+			const response = await axios.post(`${BASE_URL}/find/articles`, data);
+			if (response.data.data.length) {
+				setArticles(response.data.data);
+			} else {
+				setMessage("No results found");
+			}
+			setLoading(false);
+			setSearchTerm("");
+		} catch (error) {
+			setLoading(false);
+			setSearchTerm("");
+			console.log(error);
+		}
+	};
+
+	return (
+		<div
+			className="offcanvas offcanvas-start"
+			tabIndex="-1"
+			id="offcanvasExample"
+			aria-labelledby="offcanvasExampleLabel"
+		>
+			<div className="offcanvas-header">
+				<h5 className="offcanvas-title" id="offcanvasExampleLabel">
+					Search
+				</h5>
+				<button
+					type="button"
+					className="btn-close"
+					data-bs-dismiss="offcanvas"
+					aria-label="Close"
+				></button>
+			</div>
+			<div className="offcanvas-body">
+				<form onSubmit={(e) => searchArticles(e)}>
+					<div className="row align-items-center">
+						<div className="col-md-10">
+							<input
+								type="search"
+								name="searchTerm"
+								value={searchTerm}
+								onChange={(e) => setSearchTerm(e.target.value)}
+								className="form-control"
+								placeholder="Search..."
+							/>
+						</div>
+						<div className="col-md-2">
+							<button type="submit" className="btn btn-sm btn-dark">
+								<i className="bi bi-search"></i>
+							</button>
+						</div>
+					</div>
+				</form>
+				{message && <div className="alert alert-info my-3">{message}</div>}
+				<ul className="list-group my-3">
+					{loading ? (
+						<div className="my-3">
+							<span className="text-muted">
+								<i>Searching...</i>
+							</span>
+						</div>
+					) : (
+						articles?.map((article) => (
+							<li key={article.id} className="list-group-item">
+								<div className="d-flex justify-content-between align-items-center">
+									<img
+										src={article.image_path}
+										className="rounded me-3"
+										width={60}
+										height={60}
+										alt={article.title}
+									></img>
+									<span>{article.title}</span>
+									<Link
+										to={`/articles/${article.slug}`}
+										className="text-decoration-none text-primary"
+									>
+										View
+									</Link>
+								</div>
+							</li>
+						))
+					)}
+				</ul>
+			</div>
+		</div>
+	);
+}
